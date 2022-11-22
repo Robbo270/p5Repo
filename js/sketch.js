17a95e1efe3e20a56caeda2321fa665424e321e7
Robbo270 <116882868+Robbo270@users.noreply.github.com>
2022-11-22 10:58:52 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,58 @@
+var mySong, amplitude,  play;
+
+function preload (){
+    mySong = loadSound('assets/massiveAttackAngel.mp3');
+}
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(window.innerWidth, window.innerHeight);
     canvas.parent("p5container");
+    mySong.play();
+    background(0);
+    noStroke();
+    amplitude = new p5.Amplitude();
+    console.log(amplitude);
+
+    var button = createButton("Play");
+    button.center();
+    button.addClass('btn');
+
+    button.mousePressed(function() {
+        play = true;
+        mySong.play();
+        button.hide();
+    });
+
 }
 
 function draw() {
-    background(220);
+    if(play == true){
+        var level = amplitude.getLevel();
+        var vol = map(level, 0, 1, 0, 1000);
+        var col = map(level, 0, 1, 0, 255);
+        background(col);
+        console.log(amplitude.getLevel());
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+        fill(255, col, 0);
+        ellipse(width/2, height/2, vol);
+        
+        fill(100, 10, col);
+        ellipse(width/2, height/2, vol/2);
+    
+        rectMode(CENTER);
+        fill(col,255,255);
+        square(width/4,height/4,vol)
+    
+        rectMode(CENTER);
+        fill(col,255,255);
+        square(width-width/4,height-height/4,vol);
+        
+        rectMode(CENTER);
+        fill(col,255,255);
+        square(width-width/4,height/4,vol)
+        
+        rectMode(CENTER);
+        fill(col,255,255);
+        square(width/4,height-height/4,vol)
+    }
 }
